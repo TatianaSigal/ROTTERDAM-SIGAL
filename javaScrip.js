9cb9498fa0a01d4d896ace8d5db429bb510c6bba
TatianaSigal <tatiana.sigal@bue.edu.ar>
2022-07-03 22:30:10 -0300

    condicionales

--- javaScrip.js
+++ javaScrip.js
@@ -1,7 +1,19 @@
-let nombreUsuario = prompt("Ingrese su nombre");
 
-let edad = prompt("Ingrese su edad")
+const edadPersona= prompt("ingresa tu edad");
+const nombrePersona= prompt("ingresa tu nombre")
+let salida = nombrePersona 
 
-let salida = nombreUsuario + " " + edad + " " + "ha sido ingresado";
+if(edadPersona >=18 ) {
+    alert ("Hola" + " " + salida + " " + " A disfrutar de Rotterdam Devoto");
+    if (edadPersona >=18){
+    }
+    if( edadPersona == 40){ 
+        alert(salida + " " + "Con tu entrada Tenés un trago gratis");}
+    }
+    
+   else{ 
+    alert( salida + " " + "Todavía NO podés venir a Rotterdam ...te esperamos para festejar tus 18");
+}
+    
 
-alert(salida);
+    
